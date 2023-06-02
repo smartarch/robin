4426c8d90fc89cb7272a7944895b1fd13760e556
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2023-06-02 16:58:48 +0200

    Add class based admin view for models

--- interface/publication/admin.py
+++ interface/publication/admin.py
@@ -1,10 +1,38 @@
 from django.contrib import admin
 from .models import Affiliation, Author, Country, Keywords, Publication, Event, Source
 
-admin.site.register(Affiliation)
-admin.site.register(Author)
-admin.site.register(Country)
-admin.site.register(Keywords)
-admin.site.register(Publication)
-admin.site.register(Source)
-admin.site.register(Event)
+@admin.register(Affiliation)
+class AffiliationAdmin(admin.ModelAdmin):
+    list_display = ("institute","country")
+    search_fields = ("institute","country")
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "affiliation")
+    search_fields = ("first_name", "last_name", "affiliation")
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ("name", "other_forms")
+    search_fields = ("name", "other_forms")
+
+
+@admin.register(Keywords)
+class KeywordsAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+@admin.register(Publication)
+class PublicationAdmin(admin.ModelAdmin):
+    list_display = ("doi", "title")
+    search_fields = ("doi", "title", "abstract", "keywords", "author")
+
+@admin.register(Source)
+class SourceAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("name", "publisher", "type")
+    search_fields = ("name", "publisher", "type")
