53308d1b57ed485c7de5e0b820d1fa74b150c74b
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2023-06-06 02:19:56 +0200

    Add query models

--- interface/query/models.py
+++ interface/query/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from review.models import Review
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Query(models.Model):
+	text = models.TextField()
+	review = models.ForeignKey(Review, on_delete=models.CASCADE)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Platform(models.Model):
+	name = models.CharField(max_length=128)
+
+
+class QueryExecution(models.Model):
+	platform = models.ForeignKey(Platform, on_delete=models.PROTECT)
+	query = models.ForeignKey(Query, on_delete=models.CASCADE)
+	results = models.TextField()
+	executed_at = models.DateTimeField(auto_now_add=True)
