06bd109092420fb07dea7c7ffdac544620c9a067
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2023-06-06 02:19:49 +0200

    Add review models

--- interface/review/models.py
+++ interface/review/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from publication.models import Publication
 
-# Create your models here.
+
+class PublicationList(models.Model):
+	name = models.CharField(max_length=128)
+	publications = models.ManyToManyField(Publication)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+
+class Review(models.Model):
+	leader = models.ForeignKey(User, on_delete=models.PROTECT)
+	reviewers = models.ManyToManyField(User, related_name="reviewer_users")
+	publication_lists = models.ManyToManyField(PublicationList)
+	secret_key = models.SlugField(max_length=50)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+
+class Journal(models.Model):
+	review = models.ForeignKey(Review, on_delete=models.CASCADE)
+	user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+	message = models.TextField(blank=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
