762ae07b5168cfba67bd3d5b5817a4cad2617ebd
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2023-06-10 01:03:03 +0200

    Add list URLs to the main gateway

--- interface/interface/urls.py
+++ interface/interface/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from publication.views import  HomeView, PublicationView, AddSinglePublication
+from publication.views import HomeView, PublicationView, AddSinglePublication
+from review.views import NewReviewView, JoinReviewView, AllReviewsView, ReviewView, ReviewDeleteView
+from review.list_views import NewListView, CopyListView, ReviewAllListView, ReviewListView, ListDeleteView
 from .views import PublicView, DashboardView, DashboardAccountView
 
 urlpatterns = [
@@ -25,6 +27,23 @@ urlpatterns = [
     path('', PublicView.as_view(), name="public"),
     path('dashboard', DashboardView.as_view(), name="dashboard"),
     path('dashboard/account', DashboardAccountView.as_view(), name="dashboard_account"),
+
+    # review views
+    path('dashboard/review/all', AllReviewsView.as_view(), name="dashboard_all_reviews"),
+    path('dashboard/review/new', NewReviewView.as_view(), name="dashboard_review_new"),
+    path('dashboard/review/join', JoinReviewView.as_view(), name="dashboard_review_join"),
+    path('dashboard/review/<int:review_id>/', ReviewView.as_view(), name="dashboard_review"),
+    path('dashboard/review/<int:review_id>/delete', ReviewDeleteView.as_view(), name="dashboard_review_delete"),
+
+    path('dashboard/review/<int:review_id>/list/all', ReviewAllListView.as_view(), name="dashboard_review_all_lists"),
+    path('dashboard/review/<int:review_id>/list/new', NewListView.as_view(), name="dashboard_review_new_list"),
+    path('dashboard/review/<int:review_id>/list/copy/<int:list_id>/', CopyListView.as_view(),
+         name="dashboard_review_copy_list"),
+    path('dashboard/review/<int:review_id>/list/<int:list_id>/', ReviewListView.as_view(),
+         name="dashboard_review_list"),
+    path('dashboard/review/<int:review_id>/list/<int:list_id>/delete', ListDeleteView.as_view(),
+         name="dashboard_review_list_delete"),
+
     path('publications/<slug:pk>/', PublicationView.as_view(), name="publications"),
     path('publications/add', AddSinglePublication.as_view(), name="add_single_publication"),
 ]
