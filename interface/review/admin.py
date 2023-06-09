b453f754265cc6e24b2f5fb442579ee1c7f98c98
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2023-06-10 01:03:48 +0200

    Remove Journal model

--- interface/review/admin.py
+++ interface/review/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Review, PublicationList, UserPreferences
 
-# Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("name", "leader")
+    search_fields = ("name", "leader", "description")
+
+
+@admin.register(PublicationList)
+class PublicationListAdmin(admin.ModelAdmin):
+    list_display = ("name", "review")
+    search_fields = ("name", "review")
+
+
+@admin.register(UserPreferences)
+class UserPreferencesAdmin(admin.ModelAdmin):
+    list_display = ("user", "default_review", "default_list", "default_page_size")
+    search_fields = ("name",)
--- interface/review/models.py
+++ interface/review/models.py
@@ -3,25 +3,32 @@ from django.contrib.auth.models import User
 from publication.models import Publication
 
 
+class Review(models.Model):
+	name = models.CharField(max_length=128)
+	description = models.TextField(blank=True)
+	leader = models.ForeignKey(User, on_delete=models.PROTECT)
+	reviewers = models.ManyToManyField(User, related_name="reviewer_users")
+	secret_key = models.SlugField(max_length=255)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return f"{self.name} by {self.leader.username}"
+
+
 class PublicationList(models.Model):
 	name = models.CharField(max_length=128)
+	review = models.ForeignKey(Review, on_delete=models.CASCADE)
 	publications = models.ManyToManyField(Publication)
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 
-
-class Review(models.Model):
-	leader = models.ForeignKey(User, on_delete=models.PROTECT)
-	reviewers = models.ManyToManyField(User, related_name="reviewer_users")
-	publication_lists = models.ManyToManyField(PublicationList)
-	secret_key = models.SlugField(max_length=50)
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
+	def __str__(self):
+		return f"{self.name} for {self.review}"
 
 
-class Journal(models.Model):
-	review = models.ForeignKey(Review, on_delete=models.CASCADE)
-	user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-	message = models.TextField(blank=True)
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
+class UserPreferences(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	default_review = models.ForeignKey(Review, on_delete=models.SET_NULL, null=True)
+	default_list = models.ForeignKey(PublicationList, on_delete=models.SET_NULL, null=True)
+	default_page_size = models.PositiveSmallIntegerField(default=25)
