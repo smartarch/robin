a27c1d3b7e8977729c4094a6d721da752910415c
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2023-06-30 20:16:17 +0200

    Reform Query Model

--- interface/query/models.py
+++ interface/query/models.py
@@ -1,10 +1,21 @@
 from django.db import models
-from mapping.models import PublicationList
+from publication.models import Publication
 from django.contrib.auth.models import User
 
 
-class Query(models.Model):
-	text = models.TextField()
-	publication_list = models.ForeignKey(PublicationList, on_delete=models.CASCADE)
-	created_at = models.DateTimeField(auto_now_add=True)
+class QueryPlatform (models.Model):
+	key = models.CharField(max_length=2048)
+	source = models.CharField(max_length=1024)
+	params = models.TextField()
+	url = models.URLField(null=True)
+	help_link = models.URLField(null=True)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+
+class Query(models.Model):
+	query = models.TextField()
+	results = models.JSONField(null=True)
+	found_publications = models.ManyToManyField(Publication)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	platform = models.ForeignKey(QueryPlatform, on_delete=models.SET_NULL ,null=True)
+	created_at = models.DateTimeField(auto_now_add=True)
