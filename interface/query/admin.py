5e6216671e02d29027e0884f5fead941c228494d
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2023-06-30 20:16:05 +0200

    Add Query in admin panel

--- interface/query/admin.py
+++ interface/query/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import QueryPlatform, Query
 
-# Register your models here.
+
+@admin.register(Query)
+class QueryAdmin(admin.ModelAdmin):
+    list_display = ("query", "platform", "created_at")
+    search_fields = ("query", )
+
+
+@admin.register(QueryPlatform)
+class QueryPlatformAdmin(admin.ModelAdmin):
+    list_display = ("source", "user")
+    search_fields = ("source",)
