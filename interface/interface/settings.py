49c3a746c9dca4af7e81bb472d774cc959a939de
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2023-07-03 23:45:52 +0200

    Disable Debug Mode

--- interface/interface/settings.py
+++ interface/interface/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b6do=j0(&r=9=&i)%7dctdzs4w-v31#v(ezqk_o5va%g_euzlu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
