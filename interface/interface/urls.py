038371544028e74ec97defacf7b1b1768b635249
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2023-07-03 23:48:22 +0200

    Add error URLs

--- interface/interface/urls.py
+++ interface/interface/urls.py
@@ -48,3 +48,8 @@ urlpatterns = [
     path('publications/add/web/<int:list_id>/', AddPublicationsByWeb.as_view(), name="add_publication_by_web"),
     path('publications/add/doi', AddPublicationByDOI.as_view(), name="add_publication_by_doi"),
 ]
+
+handler404 = "interface.views.not_found"
+handler500 = "interface.views.server_error"
+handler400 = "interface.views.bad_request"
+handler403 = "interface.views.access_denied"
