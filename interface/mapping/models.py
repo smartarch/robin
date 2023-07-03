57c5c5223893f618031df81a41b190511c5cdd20
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2023-07-03 23:55:12 +0200

    Add auto-follow to the lists

--- interface/mapping/models.py
+++ interface/mapping/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from publication.models import Publication
-
 from .criteria import create_advanced_query
 
 class Mapping(models.Model):
@@ -21,26 +20,24 @@ class PublicationList(models.Model):
 	name = models.CharField(max_length=128)
 	mapping = models.ForeignKey(Mapping, on_delete=models.CASCADE)
 	publications = models.ManyToManyField(Publication)
-	subscriptions = models.ManyToManyField("PublicationList")
+	followers = models.ManyToManyField("PublicationList", related_name="my_followers")
+	subscriptions = models.ManyToManyField("PublicationList", related_name="my_subscriptions")
 	criteria = models.TextField(blank=True)
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 
-	def collect(self):
-		if self.criteria:
-			try:
-				query = create_advanced_query(self.criteria)
-				collected = None
-				for subscription in self.subscriptions.all():
-					if collected:
-						collected = collected | subscription.publications.filter(query)
-					else:
-						collected = subscription.publications.filter(query)
-				return collected
-			except BufferError:
-				return None
-		else:
-			return self.publications
+	def save(self, force_insert=False, force_update=False, using=None, update_fields=None ):
+		if self.id:
+			for follower in self.followers.all():
+				query = create_advanced_query(follower.criteria)
+				filtered_publications = self.publications.filter(query)
+				if filtered_publications:
+					for pub in filtered_publications:
+						if pub not in follower.publications.all():
+							follower.publications.add(pub)
+							follower.save()
+
+		super().save(force_insert, force_update, using, update_fields)
 
 	def __str__(self):
 		return f"{self.name} for {self.mapping}"
