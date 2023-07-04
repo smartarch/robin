b030190347d2c29ab84bc78eb2ffed261b728a6a
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2023-07-04 10:03:11 +0200

    Enable Debug=True for review process

--- interface/interface/settings.py
+++ interface/interface/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b6do=j0(&r=9=&i)%7dctdzs4w-v31#v(ezqk_o5va%g_euzlu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -144,5 +144,4 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
