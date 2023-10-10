1574c93236e33a0bcaca113c48500eebda0bf74f
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2023-10-10 14:11:04 +0200

    Update URLS

--- interface/interface/urls.py
+++ interface/interface/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from publication.views import AddPublicationByDOI, AddPublicationsByBibText, AddPublicationsByWeb
-from mapping.views import NewMappingView, JoinMappingView, AllMappingsView, MappingView, MappingDeleteView
-from mapping.list_views import NewListView, MappingAllListView, MappingListView, ListDeleteView
+from django.conf.urls.static import static
+from django.conf import settings
+from publication.views import AddPublicationByDOI, AddPublicationsByWeb
 from .views import PublicView, DashboardView, DashboardAccountView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,25 +30,12 @@ urlpatterns = [
     path('dashboard/account', DashboardAccountView.as_view(), name="dashboard_account"),
 
     # mapping views
-    path('dashboard/all_mappings/', AllMappingsView.as_view(), name="dashboard_all_mappings"),
-    path('dashboard/new_mapping/', NewMappingView.as_view(), name="dashboard_mapping_new"),
-    path('dashboard/join_mapping/', JoinMappingView.as_view(), name="dashboard_mapping_join"),
-    path('dashboard/view_mapping/<int:mapping_id>/', MappingView.as_view(), name="dashboard_mapping"),
-    path('dashboard/delete_mapping/<int:mapping_id>/', MappingDeleteView.as_view(), name="dashboard_mapping_delete"),
+    path("dashboard/mapping/", include("mapping.urls")),
 
-    path('dashboard/mapping/<int:mapping_id>/all_lists/', MappingAllListView.as_view(),
-         name="dashboard_mapping_all_lists"),
-    path('dashboard/mapping/<int:mapping_id>/new_list/', NewListView.as_view(), name="dashboard_mapping_new_list"),
-
-    path('dashboard/mapping/<int:mapping_id>/view_list/<int:list_id>/', MappingListView.as_view(),
-         name="dashboard_mapping_list"),
-    path('dashboard/mapping/<int:mapping_id>/delete_list/<int:list_id>/', ListDeleteView.as_view(),
-         name="dashboard_mapping_list_delete"),
-
-    path ('publications/add/bib_text/', AddPublicationsByBibText.as_view(), name="add_publication_by_bib_text"),
     path('publications/add/web/<int:list_id>/', AddPublicationsByWeb.as_view(), name="add_publication_by_web"),
     path('publications/add/doi/', AddPublicationByDOI.as_view(), name="add_publication_by_doi"),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = "interface.views.not_found"
 handler500 = "interface.views.server_error"
