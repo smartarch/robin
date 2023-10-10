402c50ce75e68bd3871425fb15c07b20ee0c203d
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2023-10-10 14:18:25 +0200

    Update mapping files

---
+++ interface/mapping/urls.py
@@ -0,0 +1,29 @@
+# django packages
+from django.urls import path
+
+# local app
+from .field_views import NewFieldView, EditFieldsView ,FieldReviewView
+from .reviewer_views import NewReviewView, EditReviewView
+from .list_views import MappingAllListView, NewListView, MappingListView, ListDeleteView
+from .views import AllMappingsView, NewMappingView, JoinMappingView, MappingView, MappingDeleteView
+
+
+urlpatterns = [
+    path('all/', AllMappingsView.as_view(), name="mapping_all"),
+    path('new/', NewMappingView.as_view(), name="mapping_new"),
+    path('join/', JoinMappingView.as_view(), name="mapping_join"),
+    path('view/<int:mapping_id>/', MappingView.as_view(), name="mapping"),
+    path('delete_mapping/<int:mapping_id>/', MappingDeleteView.as_view(), name="mapping_delete"),
+
+    path('view/<int:mapping_id>/lists/all', MappingAllListView.as_view(),name="publication_list_all"),
+    path('view/<int:mapping_id>/lists/new', NewListView.as_view(), name="publication_list_new"),
+    path('view/<int:mapping_id>/lists/view/<int:list_id>/', MappingListView.as_view(), name="publication_list"),
+    path('view/<int:mapping_id>/lists/delete/<int:list_id>/', ListDeleteView.as_view(), name="publication_list_delete"),
+
+    path("view/<int:mapping_id>/lists/view/<int:list_id>/field/new", NewFieldView.as_view(), name="field_new"),
+    path("view/<int:mapping_id>/lists/view/<int:list_id>/field/edit", EditFieldsView.as_view(), name="field_edit"),
+    path("view/<int:mapping_id>/lists/view/<int:list_id>/field/review", FieldReviewView.as_view(), name="field_review"),
+
+    path("view/<int:mapping_id>/lists/view/<int:list_id>/review/new", NewReviewView.as_view(), name="review_new"),
+    path("view/<int:mapping_id>/lists/view/<int:list_id>/review/edit", EditReviewView.as_view(), name="review_edit"),
+]
