e82d38e9cf5a6203113ee38e64ee587971e7c488
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2024-04-18 00:23:23 +0300

    Add features to Models

--- server/publication/models.py
+++ server/publication/models.py
@@ -2,29 +2,34 @@ from django.db import models
 from .constants import VENUE_TYPES, FULL_TEXT_TYPES, FULL_TEXT_STATUS
 
 
-class Source(models.Model):
+class TimeAwareModel(models.Model):
+	first_created = models.DateTimeField(auto_now_add=True)
+	last_update = models.DateTimeField(auto_now=True)
+
+
+class Source(TimeAwareModel):
 	"""
 		the source of the publication such as ACM, IEEE, ... etc.
 	"""
 	name = models.CharField(max_length=128)
 
 
-class Country(models.Model):
+class Country(TimeAwareModel):
 	"""
 		Country of origin (in time of publication) for authors' affiliations
 	"""
 	name = models.CharField(max_length=128, unique=True)
 
 
-class Affiliation(models.Model):
+class Affiliation(TimeAwareModel):
 	"""
 		Affiliation of the authors
 	"""
 	institute = models.CharField(max_length=256)
-	country = models.ForeignKey(Country, on_delete=models.SET_NULL, null=True)
+	country = models.ForeignKey(Country, on_delete=models.SET_NULL, null=True, related_name='institutes')
 
 
-class Author(models.Model):
+class Author(TimeAwareModel):
 	"""
 		Represents the author of the publications.
 	"""
@@ -32,11 +37,11 @@ class Author(models.Model):
 	last_name = models.CharField(max_length=256)
 
 	# optional fields
-	affiliation = models.ForeignKey(Affiliation, on_delete=models.SET_NULL, null=True)
+	affiliation = models.ForeignKey(Affiliation, on_delete=models.SET_NULL, null=True, related_name="authors")
 	ORCID = models.SlugField(max_length=1024, null=True)
 
 
-class Venue(models.Model):
+class Venue(TimeAwareModel):
 	"""
 		Represents the venue of the publication
 	"""
@@ -49,14 +54,14 @@ class Venue(models.Model):
 	issue = models.CharField(max_length=128)
 
 
-class Keywords(models.Model):
+class Keyword(TimeAwareModel):
 	"""
 		Represents the keywords assigned to publications
 	"""
 	keyword = models.CharField(max_length=256, unique=True)
 
 
-class Publication(models.Model):
+class Publication(TimeAwareModel):
 	"""
 		The actual publication object
 	"""
@@ -65,20 +70,20 @@ class Publication(models.Model):
 	year = models.PositiveSmallIntegerField()
 	datetime_added = models.DateTimeField(auto_now=True)
 
-	venue = models.ForeignKey(Venue, on_delete=models.PROTECT)
+	venue = models.ForeignKey(Venue, on_delete=models.PROTECT, related_name="papers")
 	authors = models.ManyToManyField(Author)
-	keywords = models.ManyToManyField(Keywords)
+	keywords = models.ManyToManyField(Keyword)
 
 	# optional fields
-	source = models.ForeignKey(Source, on_delete=models.SET_NULL, null=True)
+	source = models.ForeignKey(Source, on_delete=models.SET_NULL, null=True, related_name="publications")
 	abstract = models.TextField(blank=True)
 
 
-class FullText(models.Model):
+class FullText(TimeAwareModel):
 	"""
 		Represents the full text files
 	"""
-	publication = models.ForeignKey(Publication, on_delete=models.CASCADE)
+	publication = models.ForeignKey(Publication, on_delete=models.CASCADE, related_name="full_texts")
 	type = models.CharField(max_length=1, default="T", choices=FULL_TEXT_TYPES)
 	status = models.CharField(max_length=1, default="E", choices=FULL_TEXT_STATUS)
 	file = models.FileField(upload_to="full_text", blank=True)
