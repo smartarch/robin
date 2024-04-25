cc1c0380d2cd8e867199be9a94283c2d9d9294d4
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2024-04-25 14:49:53 +0200

    Add edit and parser views

--- server/publication/urls.py
+++ server/publication/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path, include
 
 urlpatterns = [
-	path("view/", include("publication.view_urls")),
-	# path("insert/", include("publication.insert_urls")),
-	# path("update/", include("publication.update_urls")),
-	# path("delete/", include("publication.delete_urls"))
+	path("view/", include("publication.model_view_urls")),
+	path("edit/", include("publication.model_edit_urls")),
+	path("parse/", include("publication.parser_urls")),
 ]
