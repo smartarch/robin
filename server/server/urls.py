c82d96989388d04f3bc69eda861cb856212ff488
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2024-04-25 14:50:11 +0200

    Add user urls

--- server/server/urls.py
+++ server/server/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("", include("publication.urls")),
+    path('reviewer/', include("reviewer.urls")),
+    path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
 ]
