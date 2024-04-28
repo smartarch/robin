a2ace28a3bc9bc49b4ef1baa42e7e4eaedb18ebf
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2024-04-28 11:34:44 +0200

    Register models for admin

--- server/publication/admin.py
+++ server/publication/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Source, Venue, Publication, Affiliation, Author, Keyword, FullText, Country
 # Register your models here.
+
+admin.site.register(Source)
+admin.site.register(Country)
+admin.site.register(Affiliation)
+admin.site.register(Author)
+admin.site.register(Keyword)
+admin.site.register(Venue)
+admin.site.register(Publication)
+admin.site.register(FullText)
+
+
