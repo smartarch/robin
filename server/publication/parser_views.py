8827f73cf35af52820fe139f9fc0d19aa700b3a4
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2024-04-28 11:35:11 +0200

    Add first doi-parser view

--- server/publication/parser_views.py
+++ server/publication/parser_views.py
@@ -1,12 +1,12 @@
 from typing import Any
 
-from django.core.exceptions import BadRequest
+from django.core.exceptions import BadRequest, ObjectDoesNotExist
 from rest_framework import generics, mixins
 from rest_framework.authentication import TokenAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-import serializers
+from . import serializers
 from .models import Publication
 from .parsers import DOIParser
 
@@ -20,13 +20,20 @@ class ParseDOIView(mixins.ListModelMixin, generics.GenericAPIView):
 		if 'doi' not in request.POST:
 			raise BadRequest("Missing doi in POST data.")
 
-		doi_parser = DOIParser()
 		doi_list = request.POST.getlist("doi")
 
 		publication_ids = []
 		for doi in doi_list:
-			publication = doi_parser.parse(doi)
-			publication_ids.append(publication.id)
+			# first, check if the DOI already exist in the database
+			try:
+				publication = self.queryset.get(doi=doi)
+			except ObjectDoesNotExist:
+				doi_parser = DOIParser(doi)
+				print (doi_parser)
+				publication = doi_parser.parse()
+
+			if publication:
+				publication_ids.append(publication.id)
 
 		self.queryset.filter(id__in=publication_ids)
 		return self.list(request, *args, **kwargs)
