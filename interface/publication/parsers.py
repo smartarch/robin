f2725f96bd4e1f28a3ef0f9496c914911a6c5eb8
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2024-05-07 12:49:48 +0200

    Enable Web Search parsers

--- interface/publication/parsers.py
+++ interface/publication/parsers.py
@@ -1,80 +1,133 @@
-# import requests
-# import re
-# from xml.parsers.expat import ExpatError
-# import xmltodict
-# import re
-# from typing import Any
-#
-# ARTICLE_TYPES = {
-# 	"article": "Journal Article",
-# 	"Early Access Articles": "Article",
-# 	"book": "(Whole) Book",
-# 	"Book": "(Whole) Book",
-# 	"Book Series": "(Whole) Book",
-# 	"booklet": "(Whole) Booklet",
-# 	"conference": "Conference Paper",
-# 	"Conferences": "Conference Paper",
-# 	"conference-proceeding": "Conference Paper",
-# 	"Conference Proceeding": "Conference Paper",
-# 	"proceedings-article": "Conference Paper",
-# 	"inbook": "Book Section",
-# 	"incollection": "Collection Paper",
-# 	"inproceedings": "Conference Paper",
-# 	"Journal-article": "Journal Article",
-# 	"journal-article": "Journal Article",
-# 	"Journal": "Journal Article",
-# 	"Journals": "Journal Article",
-# 	"manual": "Technical Manual",
-# 	"masterthesis": "Master Thesis",
-# 	"misc": "Miscellaneous",
-# 	"phdthesis": "Ph.D Thesis",
-# 	"proceedings": "(Whole) Conference Proceeding",
-# 	"techreport": "Technical Report",
-# 	"unpublished": "Unpublished",
-# 	"other": "other",
-# 	"Magazines": "Magazines",
-# 	None: None,
-# }
-#
-#
-# def find_article_type(key):
-# 	if key in ARTICLE_TYPES:
-# 		return ARTICLE_TYPES[key]
-# 	else:
-# 		return "other"
-#
-#
-# class Parser:  # generic parser
-# 	def parse_authors(self, text: Any) -> Any:
-# 		raise NotImplemented
-#
-# 	def parse_affiliation(self, text: Any) -> Any:
-# 		raise NotImplemented
-#
-# 	def parse(self, text: Any) -> Any:
-# 		raise NotImplemented
-#
-# 	@staticmethod
-# 	def try_get(msg: dict, key: str) -> Any:
-# 		if msg and key in msg:
-# 			return msg[key]
-# 		else:
-# 			return None
-#
-# 	@staticmethod
-# 	def try_access(msg: list, index: int) -> Any:
-# 		if msg and index < len(msg):
-# 			return msg[index]
-# 		else:
-# 			return None
-#
-# 	@staticmethod
-# 	def try_lower(msg: str | None) -> Any:
-# 		if msg:
-# 			return msg.lower()
-# 		else:
-# 			return None
-#
+from typing import List, Any
+import re
+
+import requests
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import models
+from django.db.models import Model
+from requests import Response
+import xml.etree.ElementTree as eT
+
+from .constants import FULL_TEXT_TYPES_REVERSE
+from .models import Source, Country, Affiliation, Author, Venue, Keyword, Publication, FullText
+
+
+ARTICLE_TYPES = {
+	"article": "Journal Article",
+	"Early Access Articles": "Article",
+	"book": "(Whole) Book",
+	"Book": "(Whole) Book",
+	"Book Series": "(Whole) Book",
+	"booklet": "(Whole) Booklet",
+	"conference": "Conference Paper",
+	"Conferences": "Conference Paper",
+	"conference-proceeding": "Conference Paper",
+	"Conference Proceeding": "Conference Paper",
+	"proceedings-article": "Conference Paper",
+	"inbook": "Book Section",
+	"incollection": "Collection Paper",
+	"inproceedings": "Conference Paper",
+	"Journal-article": "Journal Article",
+	"journal-article": "Journal Article",
+	"Journal": "Journal Article",
+	"Journals": "Journal Article",
+	"manual": "Technical Manual",
+	"masterthesis": "Master Thesis",
+	"misc": "Miscellaneous",
+	"phdthesis": "Ph.D Thesis",
+	"proceedings": "(Whole) Conference Proceeding",
+	"techreport": "Technical Report",
+	"unpublished": "Unpublished",
+	"other": "other",
+	"Magazines": "Magazines",
+	None: None,
+}
+
+
+class GenericModelFactory:
+	model: type(models.Model)
+
+	def __init__(self, _model) -> None:
+		self.model = _model
+
+	def find_object(self, data: dict) -> models.Model | None:
+		try:
+			if self.model == Source or self.model == Country or self.model == Venue:
+				return self.model.objects.get(name=data['name'])
+
+			if self.model == Affiliation:
+				return self.model.objects.get(institute=data['institute'])
+
+			if self.model == Author:
+				return self.model.objects.filter(last_name=data['last_name']).get(first_name=data['first_name'])
+
+			if self.model == Keyword:
+				return self.model.objects.get(keyword=data['keyword'])
+
+			if self.model == Publication:
+				return self.model.objects.get(doi=data['doi'])
+
+			if self.model == FullText:
+				return self.model.objects.filter(publication=data['publication']).get(type=data['type'])
+
+			return None
+
+		except ObjectDoesNotExist:
+			print(f"object was not found for {self.model}")
+			return None
+
+	def create_or_get(self, data: dict) -> models.Model | None:
+		instance = self.find_object(data)
+		if instance is None:
+			return self.model(**data)
+
+		return instance
+
+
+
+
+def find_article_type(key):
+	if key in ARTICLE_TYPES:
+		return ARTICLE_TYPES[key]
+	else:
+		return "other"
+
+
+
+class Parser:  # generic parser
+	def parse_authors(self, text: Any) -> Any:
+		raise NotImplemented
+
+	def parse_affiliation(self, text: Any) -> Any:
+		raise NotImplemented
+
+	def parse(self) -> None | Publication:
+		raise NotImplementedError
+
+	@staticmethod
+	def try_get(msg: dict, key: str) -> Any:
+		if msg and key in msg:
+			return msg[key]
+		else:
+			return None
+
+	@staticmethod
+	def try_access(msg: list, index: int) -> Any:
+		if msg and index < len(msg):
+			return msg[index]
+		else:
+			return None
+
+	@staticmethod
+	def try_lower(msg: str | None) -> Any:
+		if msg:
+			return msg.lower()
+		else:
+			return None
+
+	def parse_text(self, text: Any) -> Any:
+		raise NotImplemented
+
 #
 # class DOIParser(Parser):
 # 	def parse_affiliation(self, author_affiliation: Any) -> dict:
@@ -205,148 +258,96 @@
 # 		return results
 #
 #
-# class IEEEXploreParser(Parser):
-# 	def parse_affiliation(self, author_affiliation: Any) -> dict:
-# 		if not author_affiliation:
-# 			return {}
-# 		code = author_affiliation.split(',')
-# 		if code:
-# 			return {
-# 				"institute": ", ".join(code[:-1]).replace('\'', ''),
-# 				"country": {
-# 					"name": code[-1].replace('\'', ''),
-# 				}
-# 			}
-# 		return {}
-#
-# 	def parse_authors(self, author_list: Any) -> Any:
-# 		authors = []
-# 		for author in author_list:
-# 			full_name = author['full_name'].split(' ')
-# 			authors.append({
-# 				"first_name": full_name[0].replace('\'', ''),
-# 				"last_name": full_name[-1].replace('\'', ''),
-# 				"affiliation": self.parse_affiliation(self.try_get(msg=author, key="affiliation"))
-# 			})
-#
-# 		return authors
-#
-# 	def parse_keywords(self, keyword_list: dict) -> list | None:
-# 		if keyword_list and "terms" in keyword_list:
-# 			return [{"name": keyword.replace('\'', '')} for keyword in keyword_list["terms"]]
-# 		return []
-#
-# 	def parse(self, text: Any) -> Any:
-# 		entities = text["articles"]
-# 		publications = []
-# 		for i, entry in enumerate(entities):
-# 			publications.append({
-# 				"source": "IEEEXplore",
-# 				"title": entry['title'].replace('\'', ''),
-# 				"clean_title": re.sub('[^a-zA-Z0-9]+', '', str(entry["title"])).lower(),
-# 				"id": i,
-# 				"doi": self.try_get(msg=entry, key='doi'),
-# 				"event": {
-# 					"name": entry["publication_title"].replace('\'', ''),
-# 					"article_type": find_article_type(self.try_get(msg=entry, key="content_type")),
-# 					"volume": self.try_get(msg=entry, key="volume"),
-# 					"number": self.try_get(msg=entry, key="issue"),
-# 					"publisher": self.try_get(msg=entry, key="publisher"),
-# 				},
-# 				"authors": {"all": self.parse_authors(entry["authors"]["authors"])},
-# 				"year": entry["publication_year"],
-# 				"abstract": self.try_get(msg=entry, key="abstract").replace('\'', ''),
-# 				"citations": self.try_get(msg=entry, key="citing_paper_count"),
-# 				"index_keywords": {
-# 					"all": self.parse_keywords(self.try_get(self.try_get(entry, "index_terms"), "ieee_terms")),
-# 				},
-# 				"author_keywords": {
-# 					"all": self.parse_keywords(self.try_get(self.try_get(entry, "index_terms"), "author_terms")),
-# 				},
-# 			})
-# 		return publications
-#
-#
-# class ScopusParser(Parser):
-# 	def parse_affiliation(self, text: Any) -> Any:
-# 		pass
-#
-# 	def parse_authors(self, text: Any) -> Any:
-# 		pass
-#
-# 	def parse(self, text: Any) -> Any:
-# 		entities = text["search-results"]["entry"]
-# 		publications = []
-# 		for i, entry in enumerate(entities):
-# 			publications.append({
-# 				"title": entry['dc:title'],
-# 				"clean_title": re.sub('[^a-zA-Z0-9]+', '', str(entry["dc:title"])).lower(),
-# 				"id": i,
-# 				"doi": self.try_get(msg=entry, key='prism:doi'),
-# 				"url": f"http://doi.org/{self.try_get(msg=entry, key='prism:doi')}",
-# 				"article_type": find_article_type(self.try_get(msg=entry, key='prism:aggregationType')),
-# 				"year": int(re.findall(".*(\d\d\d\d).*", self.try_get(msg=entry, key="prism:coverDate"))[0]),
-# 			})
-# 		return publications
-#
 
 
-from typing import List
+class IEEEXploreParser(Parser):
+	def parse_affiliation(self, author_affiliation: Any) -> dict:
+		if not author_affiliation:
+			return {}
+		code = author_affiliation.split(',')
+		if code:
+			return {
+				"institute": ", ".join(code[:-1]).replace('\'', ''),
+				"country": {
+					"name": code[-1].replace('\'', ''),
+				}
+			}
+		return {}
 
-import requests
-from django.core.exceptions import ObjectDoesNotExist
-from django.db import models
-from django.db.models import Model
-from requests import Response
-import xml.etree.ElementTree as eT
+	def parse_authors(self, author_list: Any) -> Any:
+		authors = []
+		for author in author_list:
+			full_name = author['full_name'].split(' ')
+			authors.append({
+				"first_name": full_name[0].replace('\'', ''),
+				"last_name": full_name[-1].replace('\'', ''),
+				"affiliation": self.parse_affiliation(self.try_get(msg=author, key="affiliation"))
+			})
 
-from .constants import FULL_TEXT_TYPES_REVERSE
-from .models import Source, Country, Affiliation, Author, Venue, Keyword, Publication, FullText
+		return authors
 
-
-class GenericModelFactory:
-	model: type(models.Model)
-
-	def __init__(self, _model) -> None:
-		self.model = _model
-
-	def find_object(self, data: dict) -> models.Model | None:
-		try:
-			if self.model == Source or self.model == Country or self.model == Venue:
-				return self.model.objects.get(name=data['name'])
-
-			if self.model == Affiliation:
-				return self.model.objects.get(institute=data['institute'])
-
-			if self.model == Author:
-				return self.model.objects.filter(last_name=data['last_name']).get(first_name=data['first_name'])
-
-			if self.model == Keyword:
-				return self.model.objects.get(keyword=data['keyword'])
-
-			if self.model == Publication:
-				return self.model.objects.get(doi=data['doi'])
-
-			if self.model == FullText:
-				return self.model.objects.filter(publication=data['publication']).get(type=data['type'])
-
-			return None
-
-		except ObjectDoesNotExist:
-			print(f"object was not found for {self.model}")
-			return None
-
-	def create_or_get(self, data: dict) -> models.Model | None:
-		instance = self.find_object(data)
-		if instance is None:
-			return self.model(**data)
-
-		return instance
+	def parse_keywords(self, keyword_list: dict) -> list | None:
+		if keyword_list and "terms" in keyword_list:
+			return [{"name": keyword.replace('\'', '')} for keyword in keyword_list["terms"]]
+		return []
 
 
-class Parser:
-	def parse(self) -> None | Publication:
-		raise NotImplementedError
+	def parse_text(self, text: Any) -> Any:
+		entities = text["articles"]
+		publications = []
+		for i, entry in enumerate(entities):
+			doi = self.try_get(msg=entry, key='doi'),
+			if doi is not None:
+				publications.append({
+					"source": "IEEEXplore",
+					"title": entry['title'].replace('\'', ''),
+					"clean_title": re.sub('[^a-zA-Z0-9]+', '', str(entry["title"])).lower(),
+					"id": i,
+					"doi": self.try_get(msg=entry, key='doi'),
+					"event": {
+						"name": entry["publication_title"].replace('\'', ''),
+						"article_type": find_article_type(self.try_get(msg=entry, key="content_type")),
+						"volume": self.try_get(msg=entry, key="volume"),
+						"number": self.try_get(msg=entry, key="issue"),
+						"publisher": self.try_get(msg=entry, key="publisher"),
+					},
+					"authors": {"all": self.parse_authors(entry["authors"]["authors"])},
+					"year": entry["publication_year"],
+					"abstract": self.try_get(msg=entry, key="abstract").replace('\'', ''),
+					"citations": self.try_get(msg=entry, key="citing_paper_count"),
+					"index_keywords": {
+						"all": self.parse_keywords(self.try_get(self.try_get(entry, "index_terms"), "ieee_terms")),
+					},
+					"author_keywords": {
+						"all": self.parse_keywords(self.try_get(self.try_get(entry, "index_terms"), "author_terms")),
+					},
+				})
+		return publications
+
+
+class ScopusParser(Parser):
+	def parse_affiliation(self, text: Any) -> Any:
+		pass
+
+	def parse_authors(self, text: Any) -> Any:
+		pass
+
+	def parse_text(self, text: Any) -> Any:
+		entities = text["search-results"]["entry"]
+		publications = []
+		for i, entry in enumerate(entities):
+			doi = self.try_get(msg=entry, key='doi'),
+			if doi is not None:
+				publications.append({
+					"title": entry['dc:title'],
+					"clean_title": re.sub(r'[^a-zA-Z0-9]+', '', str(entry["dc:title"])).lower(),
+					"id": i,
+					"doi": self.try_get(msg=entry, key='prism:doi'),
+					"url": f"http://doi.org/{self.try_get(msg=entry, key='prism:doi')}",
+					"article_type": find_article_type(self.try_get(msg=entry, key='prism:aggregationType')),
+					"year": int(re.findall(r".*(\d\d\d\d).*", self.try_get(msg=entry, key="prism:coverDate"))[0]),
+				})
+		return publications
 
 
 class DOIParser(Parser):
